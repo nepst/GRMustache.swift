297c67d0a3757ba77870b65205f457dd189111a9
nepst <32296079+nepst@users.noreply.github.com>
2017-09-26 13:11:13 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Mustache",
   dependencies: [
-    //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
-      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16, minor: 0)
   ],
   exclude: ["Tests/Carthage", "Tests/vendor", "Tests/Info.plist"]
 )
